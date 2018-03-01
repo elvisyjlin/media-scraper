cab26eebf816e9a21f45dfd2df7420a421544fc1
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2018-03-01 21:54:35 +0800

    add interactive scrapers

---
+++ mediascraper/interactive/instagram.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
+# Licensed under the MIT License - https://opensource.org/licenses/MIT
+
+import mediascrapers
+import os
+import sys
+
+def input_username():
+    return input('Enter a username (ENTER to exit): ').strip()
+
+if __name__ == '__main__':
+    print('Starting InstagramScraper...')
+    scraper = mediascrapers.InstagramScraper(scroll_pause = 1.0, mode='normal', debug=False)
+    if os.path.exists('credentials.json'):
+        scraper.login('credentials.json')
+    username = input_username()
+    while username != '':
+        scraper.username(username)
+        tasks = scraper.scrape()
+        scraper.download(tasks=tasks, path='download/instagram')
+        username = input_username()

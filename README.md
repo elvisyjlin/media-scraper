b03c5a76f15fe167cc6d5bbece6a8f5ec50aa5a8
Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
2018-05-17 11:39:22 +0800

    robustify tumblrer & update readme

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 # Tumblrer
 
 
-## Get Started
+## Requirement
 
 ```bash
-python3 tumblrer.py <url> --save_path=<folder>
+pip3 install -r requirements
 ```
+
+
+## Get Started
+
+##### Crawl all media in a page
+
+```bash
+python3 tumblrer.py <url_1> <url_2> <...> --save_path=<folder>
+```
+
+##### Utilize Tumblr in your program
 
 ```python
 import tumblrer
 tumblrer.path(save_path)
-tumblrer.crawl(url)
+tumblrer.crawl(urls)  # urls should be a list of urls
 ```
 
-Tumblrer skips existing files automatically.
+Note that Tumblrer skips existing files automatically.
+
+##### Show the statistics of downloaded media
+
+```bash
+python3 stat.py
+```
--- tumblrer.py
+++ tumblrer.py
@@ -1,10 +1,13 @@
 import argparse
-import requests
 import json
 import os
+import requests
 import shutil
+import time
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+from utils import log
 
 API = {
     'base': 'https://www.tumblr.com', 
@@ -20,7 +23,7 @@ VAR = {
 
 def parse():
     parser = argparse.ArgumentParser(description="Tumblrer")
-    parser.add_argument('url', help='url to crawl')
+    parser.add_argument('urls', nargs='*', help='urls to crawl')
     parser.add_argument('--save_path', default='./download', help='path to save')
     return parser.parse_args()
 
@@ -30,38 +33,69 @@ def path(path):
 def get(url, start=0, num=50):
     url = url + API['v1']['read']
     params = {'start': start, 'num': num}
+    log('Get {} with {}'.format(url, params))
     res = requests.get(url=url, params=params, verify=False)
     content = json.loads(res.text.replace('var tumblr_api_read = ', '')[:-2])
     del res
     return content
 
 def download(img_url, filename):
-    file = os.path.join(VAR['save_path'], filename)
-    if os.path.exists(file):
-        return
-    res = requests.get(img_url, stream=True)
-    with open(file, 'wb') as f:
+    if os.path.exists(filename):
+        return False
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+    log('Download {}'.format(img_url))
+    res = requests.get(img_url, stream=True, timeout=10)
+    with open(filename, 'wb') as f:
         shutil.copyfileobj(res.raw, f)
     del res
+    return True
 
 def crawl(url, start=0, num=50):
     print('Tumblrer Task', url)
     total = start + 1
     while start < total:
         content = get(url, start, num)
+        blog_name = content['tumblelog']['name']
         start = content['posts-start']
         total = content['posts-total']
         posts = content['posts']
         print('[{}/{}]'.format(start, total), '# of posts: {}'.format(len(posts)))
         for post in posts:
-            img_url = post['photo-url-1280']
-            filename = post['type'] + '/' + post['id'] + '_' + img_url.rsplit('/', 1)[1]
-            download(img_url, filename)
+            if 'photo-url-1280' in post:
+                img_url = post['photo-url-1280']
+                filename = os.path.join(VAR['save_path'], blog_name, post['type'], 
+                                        str(post['id']) + '_' + img_url.rsplit('/', 1)[1])
+                success = False
+                while not success:
+                    try:
+                        download(img_url, filename)
+                        success = True
+                    except Exception as e:
+                        print(e)
+                        print('Sleep for 1 minutes...')
+                        time.sleep(1 * 60)
+            else:
+                print(post['id'], post['url'], post['type'])
+            if 'photos' in post and post['photos'] != []:
+                print('photos', len(post['photos']))
+                for photo in post['photos']:
+                    img_url = photo['photo-url-1280']
+                    filename = os.path.join(VAR['save_path'], blog_name, post['type'], 
+                                            str(post['id']) + '_' + img_url.rsplit('/', 1)[1])
+                    success = False
+                    while not success:
+                        try:
+                            download(img_url, filename)
+                            success = True
+                        except Exception as e:
+                            print(e)
+                            print('Sleep for 1 minutes...')
+                            time.sleep(1 * 60)
         start += num
 
 if __name__ == '__main__':
     args = parse()
+    print(args)
     path(args.save_path)
-    for t in VAR['types']:
-        os.makedirs(os.path.join(VAR['save_path'], t), exist_ok=True)
-    crawl(args.url)
+    for url in args.urls:
+        crawl(url)
